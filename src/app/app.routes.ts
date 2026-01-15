2b51449420996bbe46753f4c2a399903238ea523
Mukesh <mukesh2305b@gmail.com>
2026-01-15 09:46:16 +0530

    Add files via upload

---
+++ src/app/app.routes.ts
@@ -0,0 +1,37 @@
+import { Routes } from '@angular/router';
+import { SearchResults } from './pages/search-results/search-results';
+import { Home } from './pages/home/home';
+import { ContentDetail } from './pages/content-detail/content-detail';
+import { Content } from './pages/content/content';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+
+  },
+  {
+    path: 'content/:type/:id',
+    component: ContentDetail,
+
+  },
+  {
+    path: 'category/:genres',
+    component: Content,
+
+  },
+  {
+    path: 'search',
+    component: SearchResults,
+
+  },
+  {
+    path: 'collection/:contentType',
+    component: Content,
+
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  }
+];
